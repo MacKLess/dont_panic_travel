4ce5ae7ffb9c235fb6292d751f4f48d70fe6cb19
LDMacKrell <ldmackrell@gmail.com>
2017-12-13 10:35:29 -0800

    Update app with updated review generator in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,23 @@ User.destroy_all
 
 users = []
 
-10.times do
+10.times do |n|
   character = Faker::RickAndMorty.unique.character
   password = Faker::RickAndMorty.unique.location
+  n += 1
   users.push(User.create!(name: character,
-                          email: character + '@mortiverse.gov',
-                          password: password,
-                          password_confirmation: password))
+                          email: 'email' + n.to_s + '@mortiverse.gov',
+                          password: password + password,
+                          password_confirmation: password + password))
 end
 
 rng = Random.new
 
-destinations.each do |destination|
-  dest = Destination.find_by(location: location)
+trips.each do |trip|
+  trip = Trip.find_by(location: location)
 
   3.times do
-    dest.reviews.create!(rating: rng.rand(1..5),
+    trip.reviews.create!(rating: rng.rand(1..5),
                           content: Faker::RickAndMorty.quote,
                           user: users[rng.rand(10)])
   end
